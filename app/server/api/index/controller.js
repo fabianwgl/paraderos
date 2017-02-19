d02a790361db66371e536cecfb811e96a0dd02e6
Fabian General <fabianwgl@gmail.com>
2017-02-19 02:15:40 -0300

    Slack webhook added

--- app/server/api/index/controller.js
+++ app/server/api/index/controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const request = require('request');
 const tsapi = require('transantiago-client');
 
 exports.index = (req, res) => {
@@ -13,6 +14,23 @@ exports.paradero = (req, res) => {
     tsapi(paradero).then( r => {
       var ts = r;
       var services = r.avail;
+      var ip = req.headers['x-forwarded-for'] || 
+        req.connection.remoteAddress || 
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+      //LOG
+      var payload = {
+          text: "PARADERO REQUESTED \n "+paradero+" \n IP: "+ip
+        };
+        request({
+          url: process.env.SLACK_APP_URL,
+          method: "POST",
+          json: true,
+          body: payload
+        }),(err, res, body) => {
+          console.log(res);
+      }
+
       res.render("tstgo",{
         services: services
       });
